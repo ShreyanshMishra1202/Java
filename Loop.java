7b1d0bc4487ab4862e7faaab32b1ebb21b5cd6b9
--add <shreyanshmishra1202@gmail.com>
2024-09-11 19:30:16 +0530

    Add 3 files

---
+++ Loop.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int s[]=new int[n];
+            s[0]=a+b;
+            for(int j=1;j<n;j++){
+                    s[j]=s[j-1]+(int)(Math.pow(2,j) *b);
+                    
+                }
+            for(int j=0;j<n;j++){
+                System.out.print(s[j]+" ");
+            }
+                System.out.println();
+            
+        }
+        
+        in.close();
+    }
+}
