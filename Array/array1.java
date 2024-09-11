0ac549082bb2db1ef9ed37f185c6af3b23809e3c
--add <shreyanshmishra1202@gmail.com>
2024-09-11 08:36:21 +0530

    Update

--- Array/array1.java
+++
@@ -1,8 +0,0 @@
-public class array1 {
-    public static void main(String[] args) {
-        int num[]=new int[5];
-        int y[]={3,7,5};
-        String jd[]=new String[6];
-        String sd[]={"ade","hj","fghtfh"};
-    }
-}
